40d3b2a8f239061b248710f8fe887ca2fb6ba69b
Domantas Jadenkus <djadenkus@gmail.com>
2021-10-02 20:37:43 +0300

    some levels

--- make_level_data.py
+++ make_level_data.py
@@ -6,56 +6,60 @@ img = img.convert('RGBA')
 pixdata = img.load()
 
 levels = []
-levelCount = (img.size[1] + 1) // 21
+levelCount = (img.size[1]) // 11
 print('Converting {} levels'.format(levelCount))
 
 for lvl in range(levelCount):
-	l = []
-	for y in range(20):
-		l.append(['.'] * 24)
-		for x in range(24):
-			imgx = x
-			imgy = lvl * 21 + y
-			tile = '.'
-			if pixdata[imgx, imgy] == (255, 255, 255, 255):
-				tile = '.'
-			elif pixdata[imgx, imgy] == (0, 0, 0, 255):
-				tile = '#'
-			elif pixdata[imgx, imgy] == (0, 0, 255, 255):
-				tile = '%'
-			elif pixdata[imgx, imgy] == (0, 128, 255, 255):
-				tile = '!'
-			elif pixdata[imgx, imgy] == (255, 0, 255, 255):
-				tile = '+'
-			elif pixdata[imgx, imgy] == (255, 0, 0, 255):
-				tile = '$'
-			elif pixdata[imgx, imgy] == (0, 255, 0, 255):
-				tile = '@'
-			elif pixdata[imgx, imgy] == (255, 255, 0, 255):
-				tile = '>'
-			elif pixdata[imgx, imgy] == (255, 128, 0, 255):
-				tile = 'X'
-			else:
-				print('Bad color: {}'.format(pixdata[imgx, imgy]))
-			l[y][x] = tile
-	levels.append(l)
+    l = []
+    for y in range(10):
+        l.append(['.'] * 12)
+        for x in range(12):
+            imgx = x
+            imgy = img.size[1] - lvl * 11 - y - 1
+            tile = '.'
+            if pixdata[imgx, imgy] == (255, 255, 255, 255):
+                tile = '.'
+            elif pixdata[imgx, imgy] == (0, 0, 0, 255):
+                tile = '#'
+            elif pixdata[imgx, imgy] == (128, 128, 128, 255):
+                tile = '$'
+            elif pixdata[imgx, imgy] == (0, 38, 255, 255):
+                tile = 'G'
+            elif pixdata[imgx, imgy] == (127, 0, 0, 255):
+                tile = 'X'
+            elif pixdata[imgx, imgy] == (255, 0, 0, 255):
+                tile = 'x'
+            elif pixdata[imgx, imgy] == (178, 0, 255, 255):
+                tile = ' '
+            # elif pixdata[imgx, imgy] == (255, 0, 0, 255):
+            #     tile = '$'
+            # elif pixdata[imgx, imgy] == (0, 255, 0, 255):
+            #     tile = '@'
+            # elif pixdata[imgx, imgy] == (255, 255, 0, 255):
+            #     tile = '>'
+            # elif pixdata[imgx, imgy] == (255, 128, 0, 255):
+            #     tile = 'X'
+            else:
+                print('Bad color: {}'.format(pixdata[imgx, imgy]))
+            l[y][x] = tile
+    levels.append(l)
 
 output = '{\n  "levels": ['
 needComma = False
 for lvl in levels:
-	if needComma:
-		output += ',\n'
-	else:
-		output += '\n'
-		needComma = True
-	output += '    [\n'
-	for y in range(20):
-		output += '      "' + ''.join(lvl[y])
-		if y != 19:
-			output += '",\n'
-	output += '"\n    ]'
+    if needComma:
+        output += ',\n'
+    else:
+        output += '\n'
+        needComma = True
+    output += '    [\n'
+    for y in range(10):
+        output += '      "' + ''.join(lvl[y])
+        if y != 9:
+            output += '",\n'
+    output += '"\n    ]'
 output += '\n  ]\n}\n'
 
 with open('data/levels.json', 'w') as f:
-	f.write(output)
+    f.write(output)
 print('saved')
